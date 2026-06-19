47c776e09f0ed33f1443a8bed3243d54622e9a50
KinishiyoYuan <king39396969@gmail.com>
2026-06-19 11:20:47 +0800

    Fix next.config for Next.js 16 Turbopack

--- web_app/next.config.ts
+++ web_app/next.config.ts
@@ -4,16 +4,7 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
